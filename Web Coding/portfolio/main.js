12f18d80b755b35321a6ce274a0efa2300eb1538
yyyljy <yyyljy@gmail.com>
2022-09-26 21:13:48 +0900

    20220926

---
+++ Web Coding/portfolio/main.js
@@ -0,0 +1,12 @@
+const API_KEY = "";
+
+navigator.geolocation.getCurrentPosition(
+  (position) => {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+    console.log(url);
+  },
+  () => alert("Location access not allowed.")
+);
